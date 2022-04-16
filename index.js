066ca37d52be728a89ba251c35507a07d3673905
kyra qiu <kyraqiu@gmail.com>
2022-04-16 18:54:46 -0400

    commit tree 615690ac0fb4b5095d4f3e0201a7c403ae949467

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', changeText);
+
+function changeText(){
+  document.querySelector('#text').textContent = 'This is really cool!';
+}  
+
